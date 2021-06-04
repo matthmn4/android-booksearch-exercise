43b981915d8e30cf53217adb504e3e5997326630
Matthew Nguyen <matthewnguyen2019@gmail.com>
2021-06-03 18:50:18 -0700

    implemented sharing of poster sharing

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -1,28 +1,49 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.core.view.ActionProvider;
 import androidx.core.view.MenuItemCompat;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 import android.widget.ImageView;
-import android.widget.ShareActionProvider;
+import androidx.appcompat.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.models.Book;
 
 import org.parceler.Parcels;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class BookDetailActivity extends AppCompatActivity {
     private ImageView ivBookCover;
     private TextView tvTitle;
     private TextView tvAuthor;
+    private Intent shareIntent;
+
+    private ShareActionProvider miShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +59,19 @@ public class BookDetailActivity extends AppCompatActivity {
         // Use book object to populate data into views
         Glide.with(getApplicationContext())
                 .load(book.getCoverUrl())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        prepareShareIntent(((BitmapDrawable) resource).getBitmap());
+                        attachShareIntentAction();
+                        return false;
+                    }
+                })
                 .into(ivBookCover);
         tvTitle.setText(book.getTitle());
         tvAuthor.setText(book.getAuthor());
@@ -51,13 +85,48 @@ public class BookDetailActivity extends AppCompatActivity {
         mTitle.setText(book.getTitle());
     }
 
+    private void attachShareIntentAction() {
+        if (miShare != null && shareIntent != null)
+            miShare.setShareIntent(shareIntent);
+    }
+
+    private void prepareShareIntent(Bitmap bitmap) {
+        Uri bmpURI = getBitmapFromDrawable(bitmap);
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpURI);
+        shareIntent.setType("image/*");
+
+    }
+
+    private Uri getBitmapFromDrawable(Bitmap bitmap) {
+        Uri bmpUri = null;
+        try {
+            File file =  new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+
+            bmpUri = FileProvider.getUriForFile(BookDetailActivity.this, "com.codepath.fileprovider", file);  // use this version for API >= 24
+
+            // **Note:** For API < 24, you may use bmpUri = Uri.fromFile(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
         MenuItem item = menu.findItem(R.id.menu_item_share);
-        ActionProvider miShareAction = MenuItemCompat.getActionProvider(item);
+        miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        attachShareIntentAction();
         return true;
     }
 
